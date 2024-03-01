0327ec18242102c2bd8b37471de7ffaae36feaaf
JotaC <jota.crow@gmail.com>
2024-02-29 23:59:04 -0500

    FrontEnd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Home from './Pages/Home';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route></Route>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
       
     </>
   )
